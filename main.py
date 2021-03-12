5ed8871c7bf8992d493902ce15cf1dbdfae40fe9
targetretailapp <80382538+targetretailapp@users.noreply.github.com>
2021-03-11 20:10:38 -0800

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ from myretail.pricedetails import PriceDetails
 if __name__ == "__main__":
     app.run(port=5000)
 
-    # Below is temp work to see if DB part of the code is working
-    # print(db.get_price_by_product_id(13860428))
-    # p=PriceDetails()
-    p.pricedetails(13860428)
 
 
 
